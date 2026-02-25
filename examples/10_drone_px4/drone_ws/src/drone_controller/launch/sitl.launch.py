e46b3bc0e1a86bc956126bdbbf3485b541f0d4a5
Jimin Jeon <jimin@Jiminui-MacBookAir.local>
2026-02-25 14:27:33 -0500

    feat: tf fix

--- examples/10_drone_px4/drone_ws/src/drone_controller/launch/sitl.launch.py
+++ examples/10_drone_px4/drone_ws/src/drone_controller/launch/sitl.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction
+from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
@@ -7,22 +7,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     use_perception = LaunchConfiguration("use_perception")
-    force_tf_send_true = TimerAction(
-        period=2.0,
-        actions=[
-            ExecuteProcess(
-                cmd=[
-                    "bash",
-                    "-lc",
-                    "for i in {1..20}; do "
-                    "ros2 param set /mavros/local_position tf.send true && exit 0; "
-                    "sleep 0.5; "
-                    "done; exit 1",
-                ],
-                output="screen",
-            )
-        ],
-    )
 
     return LaunchDescription(
         [
@@ -42,9 +26,11 @@ def generate_launch_description():
                     {"component_id": 1},
                     {"target_system_id": 1},
                     {"target_component_id": 1},
+                    {"local_position.tf.send": True},
+                    {"local_position.tf.frame_id": "map"},
+                    {"local_position.tf.child_frame_id": "base_link"},
                 ],
             ),
-            force_tf_send_true,
             # Bridge Node
             Node(
                 package="drone_controller",
